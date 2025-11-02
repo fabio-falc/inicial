a2d8771c38317a24f2c00f420db7a81e079ac36a
falc <fabio-falc@hotmail.com>
2025-11-02 11:18:55 -0300

    terceiro commit

--- inicial.py
+++ inicial.py
@@ -2,3 +2,11 @@
 
 number = int(input("Digite um número: "))
 print(number)
+
+
+name = input("Digite o sue nome: ")
+print(f"Seu nome é {name}")
+
+altura = float(input("Digite a sua altura: "))
+
+print(f"Sua altura {name} é de {altura} cm")
